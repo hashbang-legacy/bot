84ff715396954c5fc9e9168f86a77a5eab512133
theepicsnail <theepicsnail@gmail.com>
2014-09-23 14:23:40 -0700

    Moved loader plugin from main to a separate file

--- main.py
+++ main.py
@@ -4,34 +4,8 @@ import util
 
 password = input("hashbangbot's password:")
 c = JSONIrc(connect(('localhost', 4445), '[bot]', "hashbangbot:" + password))
+plugins.loadPlugin("loader", open("loader.py").read())
 
-
-plugins.loadPlugin("loader", """
-import json, sys, pprint
-while True:
-    try:
-        line = input()
-        obj = json.loads(line)
-        msg = obj['message'].split(" ",3)
-        pprint.pprint(msg,stream=sys.stderr)
-
-        if msg[0] != '!plugin':
-            continue
-
-        name = msg[2]
-        if msg[1] == 'load':
-            print(json.dumps({
-                'type':'plugin', 'action':'load', 'name':name, 'code': msg[3]
-            }))
-        elif msg[1] == 'unload':
-            print(json.dumps({
-                'type':'plugin', 'action':'unload', 'name':name
-            }))
-        sys.stdout.flush()
-    except:
-        import traceback
-        traceback.print_exc()
-""")
 import json
 @thread
 def foo():
