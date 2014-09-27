3782e3c3ccef694881dca08a8f884b2710d0c662
theepicsnail <theepicsnail@gmail.com>
2014-09-26 21:34:27 -0700

    Pushing a different and better bot base

--- bot.py
+++ bot.py
@@ -1,6 +1,11 @@
 import socket
-import multiprocessing
+import subprocess
 from util import *
+
+def log(msg, *args):
+    msg = msg.format(*args) +" "
+    first, rest = msg.split(" ",1)
+    print("\033[33m{}\033[0m {}".format(first, rest))
 
 def parsemsg(line):
     """Breaks a message from an IRC server into its prefix, command, and
@@ -26,14 +31,73 @@ def parsemsg(line):
 class CodePlugin:
     """ Keep a singleton select loop that runs on a map of fileno->CodePlugins
     """
-    def __init__(self, handler, code):
-        print("New code plugin:", code)
+    __ID = 0
+    def __init__(self, handler, name, code):
+        CodePlugin.__ID += 1
+        name += str(CodePlugin.__ID)
+        log("[CodePlugin] New code plugin:\n[{}]", name)
+        self.handler = handler
+        self.name = name
+        self.path = "/dev/shm/" + name
+        f = open(self.path, "w")
+        f.write(code)
+        f.close()
+        self.running = False
+        self.start()
 
-        pass
+    def restart(self):
+        self.end()
+        self.start()
+
+    def start(self):
+        self.proc = subprocess.Popen(["/usr/bin/env", "python", "-u", self.path],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            cwd = "/dev/shm/")
+
+        def readThread():
+            self.running = True
+            while self.running:
+                log("{} waiting", self.name)
+                line = self.proc.stdout.readline().decode('utf-8')
+                log("{} -> {}", self.name, line)
+                if not line:
+                    log("Plugin {} returned an empty line, quitting. (restart={})", self.name, self.running)
+                    if self.running:
+                        threading.Thread(target=self.restart).start()
+                    return
+                else:
+                    line = line.strip()
+                    try:
+                        self.handler(json.loads(line))
+                    except:
+                        self.handler({"action":"message",
+                            "channel":"#test",
+                            "message": "Plugin {} output invalid json: {}"
+                                        .format(self.name, line)})
+        self.thread = threading.Thread(target=readThread)
+        self.thread.start()
+        log("started {}".format(self.name))
+
     def handleMessage(self, obj):
-        pass
+        try:
+            #log("Sending {} to {}", obj, self.name)
+            log("Sending to {}", self.name)
+            self.proc.stdin.write(json.dumps(obj).encode('utf-8') + b"\n")
+            self.proc.stdin.flush()
+        except:
+            self.restart()
+
     def end(self):
-        pass
+        self.running = False
+        try:
+            self.proc.terminate()
+        except:pass
+        log("Waiting on end of {}", self.name)
+        self.proc.wait()
+        self.thread.join()
+        log("Waiting finished  {}", self.name)
 
 class DebugPlugin:
     def __init__(self, handler):
@@ -46,23 +110,23 @@ class DebugPlugin:
 
     def handleMessage(self, obj):
         """ obj - irc message as dict """
-        if obj.get("command","") == "MODE":
-            self.seen_mode_line = True
         if self.seen_mode_line:
             self.handler({
                 "action":"message",
                 "channel":"#test",
                 "message": json.dumps(obj)})
+        if obj.get("command","") == "366":
+            self.seen_mode_line = True
 
     def end(self):
         return
+
 class PluginLoader:
     def __init__(self, handler):
         self.handler = handler
-
+    def end(self):
+        pass
     def handleMessage(self, obj):
-        print("---", obj.get('command',''))
-
         if obj.get('command','') == 'PRIVMSG':
             line = obj['args'][1].split(" ", 3)
             if line[0] == "!plugin" and len(line) >= 3: # ['!plugin', '[un]load', 'name'] + optional code
@@ -92,10 +156,11 @@ class PluginManager:
         if name in self.plugins:
             plugin = self.plugins[name]
             plugin.end()
-            print("End of", name)
+            log("PluginManager End of {}", name)
             del self.plugins[name]
 
     def loadPlugin(self, name, plugin):
+        log("Loading plugin {} into {}", name, self.plugins)
         if name in self.plugins:
             self.unloadPlugin(name)
         self.plugins[name] = plugin
@@ -111,17 +176,16 @@ class PluginManager:
             plugin.handleMessage(obj)
 
     def handlePluginMessage(self, obj):
-        print("Plugin manager:")
-        pprint.pprint(obj)
         if obj.get("action","") == "plugin":
             method = obj["method"]
             name = obj["name"]
             if method == "load":
+                log("handlePluginMessage Code: {}", obj['code'].replace("\\n", "\n"))
                 self.loadPlugin(name,
-                        CodePlugin(self.handlePluginMessage, (
-                            "#!/usr/bin/env python\n",
-                            "import json\n",
-                            "line=json.loads(input())\n",
+                        CodePlugin(self.handlePluginMessage, name, (
+                            "#!/usr/bin/env python\n" +
+                            "import json\n" +
+                            "line=json.loads(input())\n" +
                             obj["code"].replace('\\n','\n'))))
             elif method == "unload":
                 self.unloadPlugin(name)
@@ -151,8 +215,8 @@ def start():
     # setup plugin manager
 
     def handler(obj):
-        print("Plugin output:")
-        pprint.pprint(obj)
+        #log("Plugin_output:")
+        #pprint.pprint(obj)
 
         global i
         i = i -1
@@ -170,25 +234,33 @@ def start():
     pm.loadPlugin("", PluginLoader(pm.handlePluginMessage))
     # main loop
     read = ""
-    while True:
-        data = sock.recv(1024)
-        read += data.decode('utf-8')
-        lines = read.split("\r\n")
-        lines, read = lines[:-1],lines[-1]
-        for line in lines:
-            import subprocess
-            subprocess.Popen("clear").wait()
-            print(">> {}".format(line))
-            line = parsemsg(line)
+    try:
+        while True:
+            data = sock.recv(1024)
+            read += data.decode('utf-8')
+            lines = read.split("\r\n")
+            lines, read = lines[:-1],lines[-1]
+            for line in lines:
+                #import subprocess
+                #subprocess.Popen("clear").wait()
+                print(">> {}".format(line))
+                line = parsemsg(line)
 
-            if line['command'] == 'PING':
-                send("PONG {}", line['args'][0])
+                if line['command'] == 'PING':
+                    send("PONG {}", line['args'][0])
 
-            pm.handleMessage(line)
+                pm.handleMessage(line)
+    except KeyboardInterrupt:
+        log("END")
+        pm.end()
 
 
-start()
-print("Exit")
+try:
+    start()
+except:
+    print("Exceptional exit:")
+    raise
+print("Regular Exit")
 
 
 
