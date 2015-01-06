1f5c7d20ccc1f13f2e36f737c0295e76ea2a4124
theepicsnail <theepicsnail@gmail.com>
2015-01-06 13:13:33 -0500

    Reworked plugins.py to handle locks/threads/shutdown better

--- bot/plugins.py
+++ bot/plugins.py
@@ -63,14 +63,7 @@ class ScriptPlugin:
         setup a stdin for handleMessage to write to.
         """
 
-        if self.proc:
-            log("script {} killing old instance.", self.script);
-            self.proc.stdin.close()
-            self.proc.stdout.close()
-            self.proc.stderr.close()
-            self.proc.wait()
-            log("script {} killed old instance.", self.script);
-
+        log("script {} starting.", self.script);
         executable = ["bash", "start.sh"]
         self.running = True
         self.proc = subprocess.Popen(executable,
@@ -86,12 +79,22 @@ class ScriptPlugin:
             Handle end of stream """
             while self.running:
                 line = stream.readline().decode('utf-8').rstrip()
-                if not line:
-                    self.checkAlive()
-                    if not self.running:
-                        return
+                if not line: # empty line could mean process termination
 
-                    callback(line)
+                    with self.lock: # only let stderr or stdout through at a time
+
+                        if not self.running: 
+                            # if the other thread has detected the process died
+                            # don't recheck, just exit.
+                            return 
+
+                        self.checkAlive()
+
+                        if not self.running:
+                            # It was running (previous check) now it's not. quit.
+                            return
+
+                callback(line)
 
         def stdout(line):
             """ Lines from the plugins stdout go to this wrappers
@@ -109,8 +112,11 @@ class ScriptPlugin:
             """ Stderr is just piped to the bots stdout"""
             log("script {} err {}", self.script, line)
 
-        threading.Thread(target=reader, args=(self.proc.stdout, stdout)).start()
-        threading.Thread(target=reader, args=(self.proc.stderr, stderr)).start()
+        stdoutThread = lambda:reader(self.proc.stdout, stdout)
+        stderrThread = lambda:reader(self.proc.stderr, stderr)
+
+        threading.Thread(target=stdoutThread).start()
+        threading.Thread(target=stderrThread).start()
         log("script {} start ".format(self.script))
 
     def handleProcessMessage(self, message):
@@ -123,18 +129,18 @@ class ScriptPlugin:
                 "message": (self.bot.privmsg, ["channel", "message"])
         }
         # magic.
-
+        print("HandleProcessMessage", message)
         func,args = commands.get(command, (lambda :None, []))
         func(*map(message.get, args))
 
 
     def handleMessage(self, message):
         """ Send a message (dict) to the plugin process. """
-        try:
+        try: 
             self.proc.stdin.write(json.dumps(message).encode('utf-8') + b"\n")
             self.proc.stdin.flush()
         except:
-            self.bot.privmsg("#test", "script {} had a stdin exception", self.script)
+            log("script {} had a stdin exception", self.script)
             self.checkAlive()
 
     def checkAlive(self):
@@ -142,23 +148,27 @@ class ScriptPlugin:
         If the process has exited with a non-zero exit code, reload the plugin.
         """
         with self.lock:
+            traceback.print_stack()
             if not self.running:
+                log("script {} checkAlive: Wasn't running.", self.script)
                 return
 
             self.proc.poll()
             if self.proc.returncode is None:
+                log("script {} checkAlive: Running.", self.script)
                 return
 
             self.running = False
             
 
-        log("script {} exited with code {}", self.script, self.proc.returncode)
+        log("script {} checkAlive: Exited({})", self.script, self.proc.returncode)
         
-        if self.proc.returncode != 0:
-            log("script {} had an error exit code. Restarting in {}s", self.script, self.timeout)
-            #self.bot.load(ScriptPlugin(self.script))
+        if self.proc.returncode == 0:
+            log("script {} has exited with a 0 exit code. Restarting in {}s", 
+                    self.script, self.timeout)
             threading.Timer(self.timeout, self.start).start()    
         else:
+            log("script {} had an error exit code. Unloading. ", self.script)
             self.bot.unload()
 
     def __del__(self):
@@ -166,10 +176,11 @@ class ScriptPlugin:
         python /should/ call this upon shutdown.
 
         Attempt to kill the process"""
+        log("script {} __del__", self.script)
         with self.lock:
+            traceback.print_stack()
             if self.running:
                 self.proc.terminate()
-            self.proc.wait()
             self.proc.stdin.close()
             self.proc.stdout.close()
             self.proc.stderr.close()
